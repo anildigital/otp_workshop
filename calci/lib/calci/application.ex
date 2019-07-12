561554b0fd8dd60479485a24dad18963cd8a7b43
Anil Wadghule <anildigital@gmail.com>
2019-07-12 17:09:02 +0530

    Add ets support

--- calci/lib/calci/application.ex
+++ calci/lib/calci/application.ex
@@ -5,6 +5,10 @@ defmodule Calci.Application do
 
   use Application
 
+  defp init_ets() do
+    :ets.new(:calculator, [:set, :public, :named_table])
+  end
+
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: Calci.Worker.start_link(arg)
@@ -15,6 +19,7 @@ defmodule Calci.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Calci.Supervisor]
+    init_ets()
     Supervisor.start_link(children, opts)
   end
 end
--- calci/lib/calci/calculator.ex
+++ calci/lib/calci/calculator.ex
@@ -41,7 +41,7 @@ defmodule Calci.Calculator do
   # Callbacks
 
   def init(init_value) do
-    {:ok, init_value}
+    lookup()
   end
 
   def handle_call({:add, new_number}, _from, state) do
@@ -72,7 +72,9 @@ defmodule Calci.Calculator do
   # Private functions
 
   defp _add(%Calculator{value: old_value}, number) do
-    %Calculator{value: old_value + number}
+    result = old_value + number
+    store(result)
+    %Calculator{value: result}
   end
 
   defp _get(%Calculator{value: value}) do
@@ -80,14 +82,31 @@ defmodule Calci.Calculator do
   end
 
   defp _subtract(%Calculator{value: old_value}, number) do
-    %Calculator{value: old_value - number}
+    result = old_value - number
+    store(result)
+    %Calculator{value: result}
   end
 
   defp _multiply(%Calculator{value: old_value}, number) do
-    %Calculator{value: old_value * number}
+    result = old_value * number
+    store(result)
+    %Calculator{value: result}
   end
 
   defp _divide(%Calculator{value: old_value}, number) do
-    %Calculator{value: old_value / number}
+    result = old_value / number
+    store(result)
+    %Calculator{value: result}
+  end
+
+  defp store(result) do
+    :ets.insert(:calculator, {:result, %Calculator{value: result}})
+  end
+
+  defp lookup() do
+    case :ets.lookup(:calculator, :result) do
+      [{:result, result}] -> {:ok, result}
+      [] -> {:ok, %Calculator{value: 0}}
+    end
   end
 end
